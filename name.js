3bd14fee77d37af571e18710027f19870df8e21a
Sebastian Ramirez <85366730+sebastianr415@users.noreply.github.com>
2024-07-26 06:32:58 -0700

    Create name.js

---
+++ name.js
@@ -0,0 +1,13 @@
+//kevlin project from day 4
+
+//the variable kelvin cant be changed 
+const kevlin = 0
+
+//subtracting 273 from the kelvin to store into a variable called celsius 
+let celsius = kevlin - 273
+let newton = Math.floor(celsius * (33 / 100))
+//Following the rule of pemdas, dividing whats inside the parathensis, then mutiplying after addition
+//after results math.floor will round the largest integer or less 
+let fahrenheit = Math.floor(celsius * (9 / 5) + 32)
+
+console.log(`The temperature id ${fahrenheit} degrees Fah
